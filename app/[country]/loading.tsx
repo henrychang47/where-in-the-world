fbbd5edf49947b6bb8d2711c90e42614ba5bc091
henrychang47 <henry881116@gmail.com>
2023-08-11 12:14:14 +0800

    loading page

--- app/[country]/loading.tsx
+++ app/[country]/loading.tsx
@@ -1,5 +1,9 @@
 export default function loading() {
   return (
-    <div>loading..</div>
+    <div className="h-full allBgClr text-center p-4 text-2xl ">
+      <div className="animate-bounce">
+        loading..
+      </div>
+    </div>
   )
 }
